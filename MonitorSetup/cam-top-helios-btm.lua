be1a9489ff76b7a5bc4947f84119860c3e3f99e8
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-10-22 21:04:26 +0200

    Merge A-10 and KA-50 monitor setup

--- MonitorSetup/cam-top-helios-btm.lua
+++ MonitorSetup/cam-top-helios-btm.lua
@@ -83,6 +83,80 @@ MFCD_05 =
      height = 60 / CZSecondaryMonitor.height * SecondaryMonitor.height;
 }
 
+A10C_LEFT_MFCD =
+{ 
+x = 60;
+y = SecondaryMonitor.y + 120;
+width = 445;
+height = 445;
+}
+
+
+A10C_RIGHT_MFCD =
+{ 
+x = 1415;
+y = SecondaryMonitor.y + 120;
+width = 445;
+height = 445;
+}
+
+A10C_DIGITAL_CLOCK =
+{ 
+x = 478;
+y = SecondaryMonitor.y + 825;
+width = 122;
+height = 122;
+}
+
+A10C_RWR =
+{ 
+x = 600;
+y = SecondaryMonitor.y + 161;
+width = 175;
+height = 175;
+}
+
+A10C_CMSP  =
+{ 
+x = 1075;
+y = SecondaryMonitor.y + 190;
+width = 270;
+height = 70;
+}
+
+A10C_CMSC  =
+{ 
+x = 856;
+y = SecondaryMonitor.y + 141;
+width = 230;
+height = 49;
+}
+
+A10C_UHF_FRQ_STATUS =
+{ 
+x = 748;
+y = SecondaryMonitor.y + 161;
+width = 120;
+height = 30;
+}
+
+A10C_UHF_CHANNEL =
+{ 
+x = 748;
+y = SecondaryMonitor.y + 447;
+width = 30;
+height = 30;
+}
+
+
+A10C_UHF_REPEATER =
+{ 
+x = 656;
+y = SecondaryMonitor.y + 695;
+width = 110;
+height = 30;
+}
+
 Viewports =
 {
      Main =
